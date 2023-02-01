6a975540162955c704a73d471969f0f323177846
Saba Tahir <saba.tahir@cytena.com>
2023-02-01 12:13:54 +0500

    implemented TDD for counter app

---
+++ src/components/AwesomeCounter.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+const AwesomeCounter = ({ initialValue }) => {
+  const [count, setCount] = useState(initialValue ?? 0);
+  const add = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  const remove = () => {
+    setCount((prevCount) => {
+      const result = prevCount - 1;
+      if (result < 0) {
+        return 0;
+      }
+      return result;
+    });
+  };
+  return (
+    <>
+      <h1> Awesome Counter </h1>
+      <button onClick={remove}>Remove</button>
+      <span>{count}</span>
+      <button onClick={add}>Add</button>
+    </>
+  );
+};
+export default AwesomeCounter;
---
+++ src/components/AwesomeCounter.test.js
@@ -0,0 +1,61 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import AwesomeCounter from "./AwesomeCounter";
+test("it should have the correct initial value when set to 7", () => {
+  render(<AwesomeCounter initialValue={7} />);
+  const count = screen.queryByText(7);
+  expect(count).toBeVisible();
+});
+test("it should have a default value of 0", () => {
+  render(<AwesomeCounter />);
+  const count = screen.queryByText(0);
+  expect(count).toBeVisible();
+});
+test("it should increase the value correctly when add is clicked once", () => {
+  render(<AwesomeCounter initialValue={1} />);
+  const addButton = screen.getByText("Add");
+  userEvent.click(addButton);
+  const count = screen.queryByText(2);
+  expect(count).toBeVisible();
+});
+
+test("it should increase the value correctly when add is clicked twice", () => {
+  render(<AwesomeCounter initialValue={1} />);
+  const addButton = screen.getByText("Add");
+  userEvent.click(addButton);
+  userEvent.click(addButton);
+  const count = screen.queryByText(3);
+  expect(count).toBeVisible();
+});
+test("it should decrease the value correctly when remove is clicked once", () => {
+  render(<AwesomeCounter initialValue={5} />);
+  const removeButton = screen.getByText("Remove");
+  userEvent.click(removeButton);
+  const count = screen.queryByText(4);
+  expect(count).toBeVisible();
+});
+test("it should decrease the value correctly when remove is clicked twice", () => {
+  render(<AwesomeCounter initialValue={5} />);
+  const removeButton = screen.getByText("Remove");
+  userEvent.click(removeButton);
+  userEvent.click(removeButton);
+  const count = screen.queryByText(3);
+  expect(count).toBeVisible();
+});
+test("it should not allow a negative number when the initial value is 0 and remove is clicked", () => {
+  render(<AwesomeCounter initialValue={0} />);
+  const removeButton = screen.getByText("Remove");
+  userEvent.click(removeButton);
+  const count = screen.queryByText(0);
+  expect(count).toBeVisible();
+});
+test("it should not allow a negative number when the initial value is 2 and remove is clicked 4 times", () => {
+   render(<AwesomeCounter initialValue={2} />);
+   const removeButton = screen.getByText("Remove");
+   userEvent.click(removeButton);
+   userEvent.click(removeButton);
+    userEvent.click(removeButton);
+    userEvent.click(removeButton);
+   const count = screen.queryByText(0);
+   expect(count).toBeVisible();
+});
